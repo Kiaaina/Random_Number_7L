70bc817b419c1669439bc664f16e32d69b1d03c8
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-05 15:45:19 -1000

    Add files via upload

---
+++ Start_Random_7L.cmd
@@ -0,0 +1,20 @@
+cd /
+:top
+cls
+mod con: lines=6 cols=30
+title "Random_7L"
+@cecho off & cecho {09}
+@set "mil=(m_sec<9)"
+set /a max=max_number;
+set /a low=lowest_number;
+if (%mil%>%max%) & /=\
+@set "random=((%mil%/%max%)-%mil%)>%low%"
+goto top
+cecho {04}%random%{/h}
+srart "%random%.cmd"
+goto 7L
+
+:7L
+cecho {05}Made_By_Kia7L{02}
+@timeout /t 5
+goto top
