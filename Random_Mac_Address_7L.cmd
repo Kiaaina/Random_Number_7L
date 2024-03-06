3bccf055e31e85c5ecb81c6fbd713f4a7f262216
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-05 16:19:48 -1000

    Add files via upload

---
+++ Random_Mac_Address_7L.cmd
@@ -0,0 +1,20 @@
+cd /
+:top
+cls
+mod con: lines=6 cols=30
+title "Random_Mac_Address_7L"
+@cecho off & cecho {09}
+@set "mil=(m_sec<9)"
+@set "max=999999999999"
+@set "low=111111111111"
+if (%mil%>%max%) & /=\
+@set "random=((%mil%/%max%)-%mil%)>%low%"
+goto top
+cecho {04}Mac_Address{07}={02}%random%{/h}
+@[Mac_Address_%random%]
+goto 7L
+
+:7L
+cecho {05}Made_By_Kia7L{02}
+pause
+goto top
