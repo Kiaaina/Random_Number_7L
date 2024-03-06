e86d22e14f1fcc06d4482984dc998c025f82face
Kiaaina JA Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-03-05 16:02:01 -1000

    Add files via upload

---
+++ Random_Password_7L.cmd
@@ -0,0 +1,19 @@
+cd /
+:top
+cls
+mod con: lines=6 cols=30
+title "Random_Password_7L"
+@cecho off & cecho {09}
+@set "mil=(m_sec<9)"
+@set "max=99999999"
+@set "low=10000000"
+if (%mil%>%max%) & /=\
+@set "random=((%mil%/%max%)-%mil%)>%low%"
+goto top
+cecho {04}password={02}%random%{/h}
+goto 7L
+
+:7L
+cecho {05}Made_By_Kia7L{02}
+pause
+goto top
