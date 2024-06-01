cf986c9029764d50a017d97d4fab2e3a3216e73a
Kiaaina JohnAllen Medeiros <161550532+Kiaaina@users.noreply.github.com>
2024-05-31 15:29:56 -1000

    Update Random_7L.cmd

--- Random_7L.cmd
+++ Random_7L.cmd
@@ -7,12 +7,12 @@ echo off & cecho {09}
 set "mil=(m_sec<9)"
 set /a max=max_number;
 set /a low=lowest_number;
-if (%mil%>%max%) & /=\
+if %mil%>%max% & /=\
 @set "random=((%mil%/%max%)-%mil%)>%low%"
 cecho {04}%random%{\n}
 goto 7L
 
 :7L
 cecho {05}Made_By_Kia7L{02}
-@timeout /t 5
+pause
 goto top
